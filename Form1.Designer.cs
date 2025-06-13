e56a33df2026e33da0c5946439a6141797a8733c
Din Rasin <denrasin2917@gmail.com>
2025-06-13 14:48:03 +0700

    Initial commit of Bank Account Management System

---
+++ Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Back_AccountClass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitButton = new System.Windows.Forms.Button();
+            this.depositButton = new System.Windows.Forms.Button();
+            this.withdrawButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.balanceLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.depositTextBox = new System.Windows.Forms.TextBox();
+            this.withdrawTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.Red;
+            this.exitButton.Font = new System.Drawing.Font("Century", 15.75F);
+            this.exitButton.Location = new System.Drawing.Point(319, 675);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(164, 50);
+            this.exitButton.TabIndex = 0;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // depositButton
+            // 
+            this.depositButton.BackColor = System.Drawing.Color.Blue;
+            this.depositButton.Font = new System.Drawing.Font("Century", 15.75F);
+            this.depositButton.ForeColor = System.Drawing.Color.Red;
+            this.depositButton.Location = new System.Drawing.Point(164, 136);
+            this.depositButton.Name = "depositButton";
+            this.depositButton.Size = new System.Drawing.Size(164, 50);
+            this.depositButton.TabIndex = 1;
+            this.depositButton.Text = "Deposit";
+            this.depositButton.UseVisualStyleBackColor = false;
+            this.depositButton.Click += new System.EventHandler(this.depositButton_Click);
+            // 
+            // withdrawButton
+            // 
+            this.withdrawButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.withdrawButton.Font = new System.Drawing.Font("Century", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.withdrawButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.withdrawButton.Location = new System.Drawing.Point(221, 135);
+            this.withdrawButton.Name = "withdrawButton";
+            this.withdrawButton.Size = new System.Drawing.Size(164, 50);
+            this.withdrawButton.TabIndex = 2;
+            this.withdrawButton.Text = "Withdraw";
+            this.withdrawButton.UseVisualStyleBackColor = false;
+            this.withdrawButton.Click += new System.EventHandler(this.withdrawButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.depositTextBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.depositButton);
+            this.groupBox1.Font = new System.Drawing.Font("Castellar", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(29, 288);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(366, 205);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Make a Deposit";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.withdrawTextBox);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.withdrawButton);
+            this.groupBox2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.groupBox2.Font = new System.Drawing.Font("Castellar", 15.75F, System.Drawing.FontStyle.Bold);
+            this.groupBox2.Location = new System.Drawing.Point(419, 288);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(417, 205);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Make a Withdraw";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-5, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Amount :";
+            // 
+            // balanceLabel
+            // 
+            this.balanceLabel.AutoSize = true;
+            this.balanceLabel.Font = new System.Drawing.Font("Constantia", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.balanceLabel.Location = new System.Drawing.Point(400, 229);
+            this.balanceLabel.Name = "balanceLabel";
+            this.balanceLabel.Size = new System.Drawing.Size(35, 39);
+            this.balanceLabel.TabIndex = 4;
+            this.balanceLabel.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(142, 25);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Amount :";
+            // 
+            // depositTextBox
+            // 
+            this.depositTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.depositTextBox.Location = new System.Drawing.Point(154, 64);
+            this.depositTextBox.Multiline = true;
+            this.depositTextBox.Name = "depositTextBox";
+            this.depositTextBox.Size = new System.Drawing.Size(194, 38);
+            this.depositTextBox.TabIndex = 3;
+            // 
+            // withdrawTextBox
+            // 
+            this.withdrawTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.withdrawTextBox.Location = new System.Drawing.Point(191, 64);
+            this.withdrawTextBox.Multiline = true;
+            this.withdrawTextBox.Name = "withdrawTextBox";
+            this.withdrawTextBox.Size = new System.Drawing.Size(194, 39);
+            this.withdrawTextBox.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Constantia", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(246, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 33);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Account :";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Back_AccountClass.Properties.Resources.Account;
+            this.pictureBox3.Location = new System.Drawing.Point(29, 19);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(789, 196);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Back_AccountClass.Properties.Resources.Deposit;
+            this.pictureBox2.Location = new System.Drawing.Point(29, 499);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(366, 170);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Back_AccountClass.Properties.Resources.Withdraww;
+            this.pictureBox1.Location = new System.Drawing.Point(419, 499);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(417, 170);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(848, 728);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.balanceLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.exitButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button depositButton;
+        private System.Windows.Forms.Button withdrawButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox depositTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox withdrawTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label balanceLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Back_AccountClass
+{
+    public partial class Form1 : Form
+    {
+
+        private decimal balance = 0; // Starting balance
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void depositButton_Click(object sender, EventArgs e)
+        {
+            if (decimal.TryParse(depositTextBox.Text, out decimal amount))
+            {
+                if (amount > 0)
+                {
+                    balance += amount;
+                    balanceLabel.Text = $"Balance: {balance}";
+                    depositTextBox.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a positive amount to deposit.", "Invalid Input");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number for deposit.", "Invalid Input");
+            }
+        }
+
+        private void withdrawButton_Click(object sender, EventArgs e)
+        {
+            if (decimal.TryParse(withdrawTextBox.Text, out decimal amount))
+            {
+                if (amount > 0)
+                {
+                    if (amount <= balance)
+                    {
+                        balance -= amount;
+                        balanceLabel.Text = $"Balance: {balance}";
+                        withdrawTextBox.Clear();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Insufficient funds for withdrawal.", "Error");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a positive amount to withdraw.", "Invalid Input");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number for withdrawal.", "Invalid Input");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
